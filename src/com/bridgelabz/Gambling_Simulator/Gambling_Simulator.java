b634c97885fa86822595e8a8dc53c2501cc707c5
vishalshinde541 <vishalshinde541@gmail.com>
2022-11-15 23:17:05 +0530

    Win or Loose the bet

--- src/com/bridgelabz/Gambling_Simulator/Gambling_Simulator.java
+++ src/com/bridgelabz/Gambling_Simulator/Gambling_Simulator.java
@@ -4,11 +4,26 @@ import java.util.Random;
 
 public class Gambling_Simulator {
 	
+	public static int isWinOrLoose() {
+		int n1 = (int) (Math.random()*2);
+		return n1;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("----Welcome to Gambling Simulator----");
 		
 		int everyDayStake = 100;
 		int betOnEveryGame = 1;
+		
+		isWinOrLoose();
+		
+		if (isWinOrLoose() == 0) {
+			System.out.println("Gambler Win the bet of $1");
+			
+		}else {
+			System.out.println("Gambler Lost the bet of $1");
+			
+		}
 	}
 
 }
