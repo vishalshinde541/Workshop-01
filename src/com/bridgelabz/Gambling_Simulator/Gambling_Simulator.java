9d898fd4cf964be75b8d0332826b8552c319599d
vishalshinde541 <vishalshinde541@gmail.com>
2022-11-15 22:55:25 +0530

    stake of 100 every day and bet 1 every game

--- src/com/bridgelabz/Gambling_Simulator/Gambling_Simulator.java
+++ src/com/bridgelabz/Gambling_Simulator/Gambling_Simulator.java
@@ -1,5 +1,15 @@
 package com.bridgelabz.Gambling_Simulator;
 
+import java.util.Random;
+
 public class Gambling_Simulator {
+	
+	public static void main(String[] args) {
+		System.out.println("----Welcome to Gambling Simulator----");
+		
+		int everyDayStake = 100;
+		int betOnEveryGame = 1;
+		
+	}
 
 }
